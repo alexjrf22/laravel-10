72f7075017e7b9213f450efa2ab57a73493709b9
Alexandre <alexjrf22@gmail.com>
2023-09-05 16:12:45 -0300

    pest

---
+++ app/Http/Controllers/Site/SiteController.php
@@ -0,0 +1,12 @@
+<?php 
+    
+    namespace App\Http\Controllers\Site;
+
+    class SiteController
+    {
+        public function hello()
+        {
+            return view('site/hello');
+        }
+    }
+
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\admin\SupportController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Site\SiteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('supports', [SupportController::class, 'index'])->name('supports.index');
 
-Route::get('/hello', function () {
-    return print('hello world');  
+
+Route::get('/', function(){
+
+    return view('welcome');
+
 });
